229dbd1024245f130c6e0797c88eddee50da3ba2
daegool <kuma3621@naver.com>
2023-06-26 15:51:32 +0900

    branch 첫 커밋 및 location도메인 추가

---
+++ theglory-main/src/main/java/com/teamproject/theglory/kgh/domain/LocationMap.java
@@ -0,0 +1,43 @@
+package com.teamproject.theglory.kgh.domain;
+
+public class LocationMap {
+  private String locationName;
+  private String locationAddress;
+  private String tel;
+  private String latitude;
+  private String longitude;
+  
+  
+public String getLocationName() {
+	return locationName;
+}
+public void setLocationName(String locationName) {
+	this.locationName = locationName;
+}
+public String getLocationAddress() {
+	return locationAddress;
+}
+public void setLocationAddress(String locationAddress) {
+	this.locationAddress = locationAddress;
+}
+public String getTel() {
+	return tel;
+}
+public void setTel(String tel) {
+	this.tel = tel;
+}
+public String getLatitude() {
+	return latitude;
+}
+public void setLatitude(String latitude) {
+	this.latitude = latitude;
+}
+public String getLongitude() {
+	return longitude;
+}
+public void setLongitude(String longitude) {
+	this.longitude = longitude;
+}
+  
+   
+}
